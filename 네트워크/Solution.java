9103f8172f5dfc73ce882f71a9880ace262e1824
sedmz <48542327+sedmz@users.noreply.github.com>
2019-09-21 22:39:17 +0900

    Create Solution.java

---
+++ 네트워크/Solution.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+	static ArrayList<Integer>[] adj;
+	static boolean[] visited;
+
+	public int solution(int n, int[][] computers) {
+		adj = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++)
+			adj[i] = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i != j)
+					if (computers[i][j] == 1)
+						adj[i + 1].add(j + 1);
+			}
+		}
+
+		visited = new boolean[n + 1];
+    
+		int cnt = 0;
+		while (!visitAll()) {
+			int x;
+			for (x = 1; x < visited.length; x++) {
+				if (visited[x] == false)
+					break;
+			}
+			DFS(x);
+			cnt++;
+		}
+		return cnt;
+	}
+
+	public static boolean visitAll() {
+		for (int i = 1; i < visited.length; i++)
+			if (visited[i] == false)
+				return false;
+		return true;
+	}
+
+	public static void DFS(int x) {
+		visited[x] = true;
+
+		for (int y : adj[x]) {
+			if (visited[y] == false) {
+				DFS(y);
+			}
+		}
+	}
+}
