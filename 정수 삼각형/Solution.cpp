e176c80e8185fce88c68aa50ab356f31157f160c
sedmz <48542327+sedmz@users.noreply.github.com>
2019-10-12 18:20:53 +0900

    Update Solution.cpp

--- 정수 삼각형/Solution.cpp
+++ 정수 삼각형/Solution.cpp
@@ -1,37 +1,48 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-int solution(vector<vector<int>> triangle) {
-	int i, j;
-	for (i = 0; i < triangle.size(); i++) {
-		if (i == 0)
-			continue;
+int getMax(int left, int right) {
+	if (left > right)
+		return left;
+	return right;
+}
 
-		for (j = 0; j < triangle[i].size(); j++) {
-			int max;
-			if (j == 0) {
-				max = triangle[i - 1][j];
-			}
-			else if (j == triangle[i].size() - 1) {
-				max = triangle[i - 1][j - 1];
-			}
-			else {
-				int left = triangle[i - 1][j - 1];
-				int right = triangle[i - 1][j];
-				max = left > right ? left : right;
-			}
-			triangle[i][j] += max;
-		}
-
+int getMax(vector<vector<int>> triangle, int i, int j) {
+	int max;
+	if (j == 0) {
+		max = triangle[i - 1][j];
 	}
-
-	i--;
-	int max = -1;
-	for (int j = 0; j < triangle[i].size(); j++) {
-		if (triangle[i][j] > max)
-			max = triangle[i][j];
+	else if (j == triangle[i].size() - 1) {
+		max = triangle[i - 1][j - 1];
+	}
+	else {
+		int left = triangle[i - 1][j - 1];
+		int right = triangle[i - 1][j];
+		max = getMax(left, right);
 	}
 	return max;
+}
+
+int getMax(vector<vector<int>> triangle) {
+	int max = -1;
+	int lastLine = triangle.size() - 1;
+	for (int j = 0; j < triangle[lastLine].size(); j++) {
+		if (triangle[lastLine][j] > max)
+			max = triangle[lastLine][j];
+	}
+	return max;
+}
+
+int solution(vector<vector<int>> triangle) {
+	for (int i = 1; i < triangle.size(); i++) {
+		for (int j = 0; j < triangle[i].size(); j++) {
+			int max = getMax(triangle, i, j);
+			triangle[i][j] += max;
+		}
+	}
+
+	return getMax(triangle);
 }
