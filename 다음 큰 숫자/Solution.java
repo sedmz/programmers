5ceea08fb2b577adacf1c2618293fedfc1dd20ba
sedmz <48542327+sedmz@users.noreply.github.com>
2020-01-06 16:18:12 +0900

    Create Solution.java

---
+++ 다음 큰 숫자/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+	public int countOne(int n) {
+		int cnt = 0;
+		while (n > 0) {
+			if (n % 2 == 1)
+				cnt++;
+			n /= 2;
+		}
+		return cnt;
+	}
+
+	public int solution(int n) {
+		int numOfOne = countOne(n);
+
+		while (true) {
+			if (countOne(++n) == numOfOne)
+				return n;
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int answer = sol.solution(78);
+		System.out.println(answer);
+	}
+}
