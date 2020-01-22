e35a2958886ae08ca71e6f6548bbda27483fa0f9
sedmz <48542327+sedmz@users.noreply.github.com>
2020-01-22 13:29:02 +0900

    Create Solutioin.java

---
+++ 멀리 뛰기/Solutioin.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Solution {
+	public long solution(int n) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(0);
+		arr.add(1);
+		arr.add(2);
+
+		for (int i = 3; i <= n; i++) {
+			int value = arr.get(i - 1) + arr.get(i - 2);
+			value = value < 1234567 ? value : value - 1234567;
+			arr.add(value);
+		}
+		return arr.get(n);
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.solution(4));
+	}
+}
