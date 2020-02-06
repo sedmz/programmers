bc2bd3ef07d54e5dde8f59ac56b86fee25df1a58
sedmz <48542327+sedmz@users.noreply.github.com>
2020-02-06 16:47:26 +0900

    DP

--- 가장 큰 정사각형 찾기/Solution.java
+++ 가장 큰 정사각형 찾기/Solution.java
@@ -1,11 +1,10 @@
 class Solution {
 	int[][] board;
 
-	public int getMaxSize(int i, int j) {
-		int min;
-		min = Math.min(board[i][j - 1], board[i - 1][j]);
-		min = Math.min(board[i - 1][j - 1], min);
-		return min;
+	public int getSize(int i, int j) {
+		if (i > 0 && j > 0 && board[i][j] > 0)
+			return Math.min(board[i][j - 1], Math.min(board[i - 1][j - 1], board[i - 1][j]));
+		return 0;
 	}
 
 	public int solution(int[][] board) {
@@ -14,9 +13,7 @@ class Solution {
 		int max = -1;
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				if (i > 0 && j > 0)
-					if (board[i][j] > 0)
-						board[i][j] += getMaxSize(i, j);
+				board[i][j] += getSize(i, j);
 				max = Math.max(board[i][j], max);
 			}
 		}
