363ee8be497a92c0c81ae25d4c2bc3bac76153a5
sedmz <48542327+sedmz@users.noreply.github.com>
2020-08-31 09:56:19 +0900

    Update MySolution.java

--- 올바른 괄호의 갯수/MySolution.java
+++ 올바른 괄호의 갯수/MySolution.java
@@ -1,26 +1,17 @@
-import java.util.Stack;
-
 class Solution {
-	public int solution(int n) {
-		Stack<int[]> stack = new Stack<>();
-		stack.add(new int[] { n - 1, n });
-
-		int cnt = 0;
-		while (!stack.isEmpty()) {
-			int[] bracket = stack.pop();
-			int open = bracket[0], close = bracket[1];
-
-			if (open == 0 && close == 0) {
-				cnt++;
-			}
-			if (open > 0) {
-				stack.add(new int[] { open - 1, close });
-			}
-			if (close > 0 && open < close) {
-				stack.add(new int[] { open, close - 1 });
-			}
+	public int dfs(int open, int close) {
+		if (open == 0 && close == 0) {
+			return 1;
+		} else if (open < 0) {
+			return 0;
+		} else if (close < 0 || close < open) {
+			return 0;
 		}
 
-		return cnt;
+		return dfs(open - 1, close) + dfs(open, close - 1);
+	}
+
+	public int solution(int n) {
+		return dfs(n - 1, n);
 	}
 }
