645bce545276d0d36f50a10cdac416087da476ed
sedmz <48542327+sedmz@users.noreply.github.com>
2020-09-23 17:34:29 +0900

    Create MySolution.java

---
+++ 점프와 순간 이동/MySolution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+	public int solution(int n) {
+    int cnt = 0;
+    while(n > 0) {
+        if(n % 2 == 1) {
+            cnt++;
+            n--;
+        }
+        n/=2;
+    }       
+    return cnt;
+	}
+}
