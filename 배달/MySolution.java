0ed47256e1d776f2ff64ac72d7bf7d797b6af26d
sedmz <48542327+sedmz@users.noreply.github.com>
2020-09-25 23:57:47 +0900

    플로이드

---
+++ 배달/MySolution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+	public int solution(int N, int[][] road, int K) {
+		int[][] distance = new int[N][N];
+		for (int i = 0; i < distance.length; i++) {
+			Arrays.fill(distance[i], 500000);
+			distance[i][i] = 0;
+		}
+
+		for (int[] r : road) {
+			distance[r[0] - 1][r[1] - 1] = Math.min(r[2], distance[r[0] - 1][r[1] - 1]);
+			distance[r[1] - 1][r[0] - 1] = Math.min(r[2], distance[r[1] - 1][r[0] - 1]);
+		}
+
+    for (int k = 0; k < distance.length; k++) {
+      for (int i = 0; i < distance.length; i++) {
+        for (int j = 0; j < distance.length; j++) {
+					distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+				}
+			}
+		}
+
+		int cnt = 0;
+		for (int d : distance[0]) {
+			if (d <= K)
+				cnt++;
+		}
+		return cnt;
+	}
+}
