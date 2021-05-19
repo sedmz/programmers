bc276618a4449cee8cad9c2a9d02a8432fd37405
sedmz <48542327+sedmz@users.noreply.github.com>
2021-05-20 07:42:50 +0900

    Update MySolution.java

--- 약수의 개수와 덧셈/MySolution.java
+++ 약수의 개수와 덧셈/MySolution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        for(int i = left; i <= right; i++) {
+        for (int i = left; i <= right; i++) {
             if (Math.sqrt(i) % 1 == 0) {
                 answer -= i;
             } else {
